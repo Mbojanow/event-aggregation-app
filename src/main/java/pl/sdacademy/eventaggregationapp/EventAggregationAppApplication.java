af91f39739a1c98a6d1330ed5c81975d7118e81d
Michal Bojanowski <michal.bojanowski@onegini.com>
2020-12-19 12:16:23 +0100

    Initial project

---
+++ src/main/java/pl/sdacademy/eventaggregationapp/EventAggregationAppApplication.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.eventaggregationapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventAggregationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventAggregationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sdacademy/eventaggregationapp/EventAggregationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.eventaggregationapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventAggregationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
